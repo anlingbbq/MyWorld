9879cd036b75388b53154bb91a524116df4db844
anlingbbq <lijie1992no1@163.com>
2017-10-31 21:36:41 +0800

    开始优化地形

---
+++ Assets/Test/Perlin/Scripte/Chunk7Perlin.cs
@@ -0,0 +1,549 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LibNoise;
+using LibNoise.Generator;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshCollider))]
+public class Chunk7Perlin : MonoBehaviour
+{
+    // 单个chunk的模型数据
+    private List<Vector3> _vertices = new List<Vector3>();
+    private List<int> _triangles = new List<int>();
+    private List<Vector2> _uvs = new List<Vector2>();
+
+    /// <summary>
+    /// 图块贴图的长和宽
+    /// </summary>
+    public float _textureOffset = 1 / 16f;
+
+    private Mesh _mesh;
+
+    private Block[,,] _map;
+    public static int length = 16;
+    public static int width = 16;
+    public static int height = 16;
+
+    // 控制加载图快
+    public static bool working;
+    [HideInInspector]
+    public bool ready;
+
+    // 图块的格子坐标
+    public int chunkX;
+    public int chunkY;
+    public int chunkZ;
+
+    public static int seed;
+
+    // 周围的图块
+    private Chunk7Perlin _topChunk;
+    private Chunk7Perlin _bottomChunk;
+    private Chunk7Perlin _rightChunk;
+    private Chunk7Perlin _leftChunk;
+    private Chunk7Perlin _frontChunk;
+    private Chunk7Perlin _backChunk;
+
+    public void Init(int chunkX, int chunkY, int chunkZ)
+    {
+        gameObject.name = "[" + chunkX + "," + chunkY + "," + chunkZ + "]";
+        this.chunkX = chunkX;
+        this.chunkY = chunkY;
+        this.chunkZ = chunkZ;
+    }
+
+    public void CreateMap()
+    {
+        CalculateMap();
+    }
+
+    /// <summary>
+    /// 预处理地形的函数
+    /// 通过不同的算法产生地形数据
+    /// </summary>
+    private void CalculateMap()
+    {
+        _map = new Block[length, height, width];
+        for (int x = 0; x < length; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                for (int z = 0; z < width; z++)
+                {
+                    Block block = GetTheoreticalBlock(new Vector3(x, y, z) + transform.position);
+                    if (block != null)
+                    {
+                        if (GetTheoreticalBlock(new Vector3(x, y + 1, z) + transform.position) == null)
+                            _map[x, y, z] = BlockMap.GetBlock("Grass");
+                        else
+                            _map[x, y, z] = BlockMap.GetBlock("Dirt");
+                    }
+                }
+            }
+        }
+        //yield return null;
+        StartCoroutine(CalculateMesh());
+    }
+
+    Perlin noise = new Perlin(1.0f, 0.2f, 0.2f, 8, seed, QualityMode.High);
+    public Block GetTheoreticalBlock(Vector3 pos)
+    {
+        Random.InitState(seed);
+        Vector3 offset = new Vector3(Random.value * 100000, Random.value * 100000, Random.value * 100000);
+        float noiseX = Mathf.Abs(pos.x + offset.x) / 20;
+        float noiseY = Mathf.Abs(pos.y + offset.y) / 20;
+        float noiseZ = Mathf.Abs(pos.z + offset.z) / 20;
+        //float noiseValue = SimplexNoise.Noise.Generate(noiseX, noiseY, noiseZ);
+        double noiseValue = noise.GetValue(noiseX, noiseY, noiseZ);
+        noiseValue += (20.0f - pos.y) / 18;
+        noiseValue /= pos.y / 4;
+
+        return noiseValue > 0.2f ? BlockMap.GetBlock("Dirt") : null;
+    }
+
+    private IEnumerator CalculateMesh()
+    {
+        _mesh = new Mesh();
+        _mesh.name = "Chunk";
+
+        _vertices.Clear();
+        _triangles.Clear();
+        _uvs.Clear();
+
+        for (int x = 0; x < length; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                for (int z = 0; z < width; z++)
+                {
+                    if (_map[x, y, z] != null)
+                    {
+                        AddCube(x, y, z);
+                    }
+                }
+            }
+        }
+
+        _mesh.vertices = _vertices.ToArray();
+        _mesh.triangles = _triangles.ToArray();
+        _mesh.uv = _uvs.ToArray();
+
+        _mesh.RecalculateBounds();
+        _mesh.RecalculateNormals();
+        GetComponent<MeshCollider>().sharedMesh = _mesh;
+        GetComponent<MeshFilter>().mesh = _mesh;
+
+        yield return null;
+        working = false;
+        ready = true;
+    }
+
+    private IEnumerator RebuildMesh()
+    {
+        _mesh = new Mesh();
+        _mesh.name = "Chunk";
+
+        _vertices.Clear();
+        _triangles.Clear();
+        _uvs.Clear();
+
+        for (int x = 0; x < length; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                for (int z = 0; z < width; z++)
+                {
+                    if (_map[x, y, z] != null)
+                    {
+                        RebuildCube(x, y, z);
+                    }
+                }
+            }
+        }
+
+        _mesh.vertices = _vertices.ToArray();
+        _mesh.triangles = _triangles.ToArray();
+        _mesh.uv = _uvs.ToArray();
+
+        _mesh.RecalculateBounds();
+        _mesh.RecalculateNormals();
+        GetComponent<MeshCollider>().sharedMesh = _mesh;
+        GetComponent<MeshFilter>().mesh = _mesh;
+
+        yield return null;
+    }
+
+    #region 创建立方体
+    /////////////////////////////////////////////////////////////
+    /// 顶点坐标
+    /// 在左手坐标系中
+    /// 统一以左下角为第一个，左上角为最后一个的顺序添加
+    /// -----------------
+    /// |3             2|
+    /// |               |
+    /// |               |
+    /// |               |
+    /// |0             1|
+    /// -----------------
+    /// 
+    /// 纹理方向
+    /// 上下显示方向同观察的正面(z轴负方向显示的面)
+    /// 四周方向连续
+    /////////////////////////////////////////////////////////////
+    
+    private void AddCube(int x, int y, int z)
+    {
+        if (IsBlockTransparent(x, y, z - 1))
+            AddCubeFront(x, y, z);
+        if (IsBlockTransparent(x, y, z + 1))
+            AddCubeBack(x, y, z);
+        if (IsBlockTransparent(x - 1, y, z))
+            AddCubeLeft(x, y, z);
+        if (IsBlockTransparent(x + 1, y, z))
+            AddCubeRight(x, y, z);
+        if (IsBlockTransparent(x, y + 1, z))
+            AddCubeTop(x, y, z);
+        if (IsBlockTransparent(x, y - 1, z))
+            AddCubeBottom(x, y, z);
+    }
+
+    private void RebuildCube(int x, int y, int z)
+    {
+        if (IsRebuildBlockTransparent(x, y, z - 1))
+            AddCubeFront(x, y, z);
+        if (IsRebuildBlockTransparent(x, y, z + 1))
+            AddCubeBack(x, y, z);
+        if (IsRebuildBlockTransparent(x - 1, y, z))
+            AddCubeLeft(x, y, z);
+        if (IsRebuildBlockTransparent(x + 1, y, z))
+            AddCubeRight(x, y, z);
+        if (IsRebuildBlockTransparent(x, y + 1, z))
+            AddCubeTop(x, y, z);
+        if (IsRebuildBlockTransparent(x, y - 1, z))
+            AddCubeBottom(x, y, z);
+    }
+
+    /// <summary>
+    /// 处理uv的缝隙
+    /// </summary>
+    private float _shrinkSize = 0.005f;
+
+    private void AddCubeFront(int x, int y, int z)
+    {
+        _triangles.Add(0 + _vertices.Count);
+        _triangles.Add(3 + _vertices.Count);
+        _triangles.Add(2 + _vertices.Count);
+        _triangles.Add(2 + _vertices.Count);
+        _triangles.Add(1 + _vertices.Count);
+        _triangles.Add(0 + _vertices.Count);
+
+        _vertices.Add(new Vector3(-0.5f + x, -0.5f + y, -0.5f + z));
+        _vertices.Add(new Vector3(0.5f + x, -0.5f + y, -0.5f + z));
+        _vertices.Add(new Vector3(0.5f + x, 0.5f + y, -0.5f + z));
+        _vertices.Add(new Vector3(-0.5f + x, 0.5f + y, -0.5f + z));
+
+        Block block = _map[x, y, z];
+        Vector2 orgUv = new Vector2(block.texture_u_fb * _textureOffset, block.texture_v_fb * _textureOffset);
+        _uvs.Add(orgUv + new Vector2(_shrinkSize, _shrinkSize));
+        _uvs.Add(orgUv + new Vector2(_textureOffset, 0) + new Vector2(-_shrinkSize, _shrinkSize));
+        _uvs.Add(orgUv + new Vector2(_textureOffset, _textureOffset) + new Vector2(-_shrinkSize, -_shrinkSize));
+        _uvs.Add(orgUv + new Vector2(0, _textureOffset) + new Vector2(_shrinkSize, -_shrinkSize));
+    }
+
+    private void AddCubeBack(int x, int y, int z)
+    {
+        _triangles.Add(0 + _vertices.Count);
+        _triangles.Add(3 + _vertices.Count);
+        _triangles.Add(2 + _vertices.Count);
+        _triangles.Add(2 + _vertices.Count);
+        _triangles.Add(1 + _vertices.Count);
+        _triangles.Add(0 + _vertices.Count);
+
+        _vertices.Add(new Vector3(0.5f + x, -0.5f + y, 0.5f + z));
+        _vertices.Add(new Vector3(-0.5f + x, -0.5f + y, 0.5f + z));
+        _vertices.Add(new Vector3(-0.5f + x, 0.5f + y, 0.5f + z));
+        _vertices.Add(new Vector3(0.5f + x, 0.5f + y, 0.5f + z));
+
+        Block block = _map[x, y, z];
+        Vector2 orgUv = new Vector2(block.texture_u_fb * _textureOffset, block.texture_v_fb * _textureOffset);
+        _uvs.Add(orgUv + new Vector2(_shrinkSize, _shrinkSize));
+        _uvs.Add(orgUv + new Vector2(_textureOffset, 0) + new Vector2(-_shrinkSize, _shrinkSize));
+        _uvs.Add(orgUv + new Vector2(_textureOffset, _textureOffset) + new Vector2(-_shrinkSize, -_shrinkSize));
+        _uvs.Add(orgUv + new Vector2(0, _textureOffset) + new Vector2(_shrinkSize, -_shrinkSize));
+    }
+
+    private void AddCubeLeft(int x, int y, int z)
+    {
+        _triangles.Add(0 + _vertices.Count);
+        _triangles.Add(1 + _vertices.Count);
+        _triangles.Add(2 + _vertices.Count);
+        _triangles.Add(2 + _vertices.Count);
+        _triangles.Add(3 + _vertices.Count);
+        _triangles.Add(0 + _vertices.Count);
+
+        _vertices.Add(new Vector3(-0.5f + x, -0.5f + y, 0.5f + z));
+        _vertices.Add(new Vector3(-0.5f + x, 0.5f + y, 0.5f + z));
+        _vertices.Add(new Vector3(-0.5f + x, 0.5f + y, -0.5f + z));
+        _vertices.Add(new Vector3(-0.5f + x, -0.5f + y, -0.5f + z));
+
+        Block block = _map[x, y, z];
+        Vector2 orgUv = new Vector2(block.texture_u_lr * _textureOffset, block.texture_v_lr * _textureOffset);
+        _uvs.Add(orgUv + new Vector2(_shrinkSize, _shrinkSize));
+        _uvs.Add(orgUv + new Vector2(0, _textureOffset) + new Vector2(_shrinkSize, -_shrinkSize));
+        _uvs.Add(orgUv + new Vector2(_textureOffset, _textureOffset) + new Vector2(-_shrinkSize, -_shrinkSize));
+        _uvs.Add(orgUv + new Vector2(_textureOffset, 0) + new Vector2(-_shrinkSize, _shrinkSize));
+    }
+
+    private void AddCubeRight(int x, int y, int z)
+    {
+        _triangles.Add(0 + _vertices.Count);
+        _triangles.Add(3 + _vertices.Count);
+        _triangles.Add(2 + _vertices.Count);
+        _triangles.Add(2 + _vertices.Count);
+        _triangles.Add(1 + _vertices.Count);
+        _triangles.Add(0 + _vertices.Count);
+
+        _vertices.Add(new Vector3(0.5f + x, -0.5f + y, -0.5f + z));
+        _vertices.Add(new Vector3(0.5f + x, -0.5f + y, 0.5f + z));
+        _vertices.Add(new Vector3(0.5f + x, 0.5f + y, 0.5f + z));
+        _vertices.Add(new Vector3(0.5f + x, 0.5f + y, -0.5f + z));
+
+        Block block = _map[x, y, z];
+        Vector2 orgUv = new Vector2(block.texture_u_lr * _textureOffset, block.texture_v_lr * _textureOffset);
+        _uvs.Add(orgUv + new Vector2(_shrinkSize, _shrinkSize));
+        _uvs.Add(orgUv + new Vector2(_textureOffset, 0) + new Vector2(-_shrinkSize, _shrinkSize));
+        _uvs.Add(orgUv + new Vector2(_textureOffset, _textureOffset) + new Vector2(-_shrinkSize, -_shrinkSize));
+        _uvs.Add(orgUv + new Vector2(0, _textureOffset) + new Vector2(_shrinkSize, -_shrinkSize));
+    }
+
+    private void AddCubeTop(int x, int y, int z)
+    {
+        _triangles.Add(0 + _vertices.Count);
+        _triangles.Add(3 + _vertices.Count);
+        _triangles.Add(2 + _vertices.Count);
+        _triangles.Add(2 + _vertices.Count);
+        _triangles.Add(1 + _vertices.Count);
+        _triangles.Add(0 + _vertices.Count);
+
+        _vertices.Add(new Vector3(-0.5f + x, 0.5f + y, -0.5f + z));
+        _vertices.Add(new Vector3(0.5f + x, 0.5f + y, -0.5f + z));
+        _vertices.Add(new Vector3(0.5f + x, 0.5f + y, 0.5f + z));
+        _vertices.Add(new Vector3(-0.5f + x, 0.5f + y, 0.5f + z));
+
+        Block block = _map[x, y, z];
+        Vector2 orgUv = new Vector2(block.texture_u_top * _textureOffset, block.texture_v_top * _textureOffset);
+        _uvs.Add(orgUv + new Vector2(_shrinkSize, _shrinkSize));
+        _uvs.Add(orgUv + new Vector2(_textureOffset, 0) + new Vector2(-_shrinkSize, _shrinkSize));
+        _uvs.Add(orgUv + new Vector2(_textureOffset, _textureOffset) + new Vector2(-_shrinkSize, -_shrinkSize));
+        _uvs.Add(orgUv + new Vector2(0, _textureOffset) + new Vector2(_shrinkSize, -_shrinkSize));
+    }
+
+    private void AddCubeBottom(int x, int y, int z)
+    {
+        _triangles.Add(3 + _vertices.Count);
+        _triangles.Add(0 + _vertices.Count);
+        _triangles.Add(1 + _vertices.Count);
+        _triangles.Add(1 + _vertices.Count);
+        _triangles.Add(2 + _vertices.Count);
+        _triangles.Add(3 + _vertices.Count);
+
+        _vertices.Add(new Vector3(-0.5f + x, -0.5f + y, -0.5f + z));
+        _vertices.Add(new Vector3(0.5f + x, -0.5f + y, -0.5f + z));
+        _vertices.Add(new Vector3(0.5f + x, -0.5f + y, 0.5f + z));
+        _vertices.Add(new Vector3(-0.5f + x, -0.5f + y, 0.5f + z));
+
+        Block block = _map[x, y, z];
+        Vector2 orgUv = new Vector2(block.texture_u_bottom * _textureOffset, block.texture_v_bottom * _textureOffset);
+        _uvs.Add(orgUv + new Vector2(_shrinkSize, _shrinkSize));
+        _uvs.Add(orgUv + new Vector2(_textureOffset, 0) + new Vector2(-_shrinkSize, _shrinkSize));
+        _uvs.Add(orgUv + new Vector2(_textureOffset, _textureOffset) + new Vector2(-_shrinkSize, -_shrinkSize));
+        _uvs.Add(orgUv + new Vector2(0, _textureOffset) + new Vector2(_shrinkSize, -_shrinkSize));
+    }
+    #endregion
+
+    /// <summary>
+    /// 关键的优化方法，只显示边界的面
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    /// <param name="z"></param>
+    /// <returns>是否显示</returns>
+    private bool IsBlockTransparent(int x, int y, int z)
+    {
+        // 只显示矩形边界的面
+        if (x >= length || y >= height || z >= width || x < 0 || y < 0 || z < 0)
+        {
+            //return true;
+            return (GetTheoreticalBlock(new Vector3(x, y, z) + transform.position) == null);
+        }
+
+        // 显示被去除的方块产生的面
+        if (_map[x, y, z] == null)
+            return true;
+
+        return false;
+    }
+
+    /// <summary>
+    /// 修改图块后，显示的面
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    /// <param name="z"></param>
+    /// <returns>是否显示</returns>
+    private bool IsRebuildBlockTransparent(int x, int y, int z)
+    {
+        #region 检查周围是否有图块，没有则显示该面
+        Vector3 worldPos = DataUtil.FloorToInt(new Vector3(x, y, z) + transform.position);
+        // 右边
+        if (x >= length)
+        {
+            if (_rightChunk == null)
+                _rightChunk = ChunkMgr7Perlin.GetChunkByChunkPos(chunkX + 1, chunkY, chunkZ);
+            if (_rightChunk != null && _rightChunk != this && _rightChunk.ready)
+                return _rightChunk.GetBlock(worldPos) == null;
+
+            return true;
+        }
+
+        // 左边
+        if (x < 0)
+        {
+            if (_leftChunk == null)
+                _leftChunk = ChunkMgr7Perlin.GetChunkByChunkPos(chunkX - 1, chunkY, chunkZ);
+            if (_leftChunk != null && _leftChunk != this && _leftChunk.ready)
+                return _leftChunk.GetBlock(worldPos) == null;
+
+            return true;
+        }
+
+        // 前面
+        if (z < 0)
+        {
+            if (_frontChunk == null)
+                _frontChunk = ChunkMgr7Perlin.GetChunkByWorldPos(worldPos);
+            if (_frontChunk != null && _frontChunk != this && _frontChunk.ready)
+                return _frontChunk.GetBlock(worldPos) == null;
+
+            return true;
+        }
+
+        // 后面
+        if (z >= width)
+        {
+            if (_backChunk == null)
+                _backChunk = ChunkMgr7Perlin.GetChunkByWorldPos(worldPos);
+            if (_backChunk != null && _backChunk != this && _backChunk.ready)
+                return _backChunk.GetBlock(worldPos) == null;
+
+            return true;
+        }
+
+        // 上面
+        if (y >= height)
+        {
+            if (_topChunk == null)
+                _topChunk = ChunkMgr7Perlin.GetChunkByWorldPos(worldPos);
+            if (_topChunk != null && _topChunk != this && _topChunk.ready)
+                return _topChunk.GetBlock(worldPos) == null;
+
+            return true;
+        }
+
+        // 下面
+        if (y < 0)
+        {
+            if (_bottomChunk == null)
+                _bottomChunk = ChunkMgr7Perlin.GetChunkByWorldPos(worldPos);
+            if (_bottomChunk != null && _bottomChunk != this && _bottomChunk.ready)
+                return _bottomChunk.GetBlock(worldPos) == null;
+
+            return true;
+        }
+        #endregion
+
+        if (_map[x, y, z] == null)
+            return true;
+
+        return false;
+    }
+
+    public Block GetBlock(Vector3 worldPos)
+    {
+        Vector3 localPos = worldPos - transform.position;
+        return _map[Mathf.FloorToInt(localPos.x), Mathf.FloorToInt(localPos.y), Mathf.FloorToInt(localPos.z)];
+    }
+    
+    public void SetBlock(Vector3 pos, Block block)
+    {
+        Vector3 localPos = pos - transform.position;
+        int blockX = Mathf.CeilToInt(localPos.x);
+        int blockY = Mathf.CeilToInt(localPos.y);
+        int blockZ = Mathf.CeilToInt(localPos.z);
+        //print("pos: " + pos.x + ", " + pos.y + ", " + pos.z);
+        //print("local pos: " + blockX + ", " + blockY + ", " + blockZ);
+        _map[blockX, blockY, blockZ] = block;
+
+        StartCoroutine(RebuildMesh());
+        if (block != null)
+            return;
+
+        #region 重构相邻的图块，补充未显示的面
+        // 右边
+        if (blockX == length - 1)
+        {
+            if (_rightChunk == null)
+                _rightChunk = ChunkMgr7Perlin.GetChunkByChunkPos(blockX + 1, blockY, blockZ);
+            StartCoroutine(_rightChunk.RebuildMesh());
+            //Debug.Log("rihgt : " + _rightChunk.name);
+        }
+        // 左边
+        if (blockX == 0)
+        {
+            if (_leftChunk == null)
+                _leftChunk = ChunkMgr7Perlin.GetChunkByChunkPos(chunkX - 1, chunkY, chunkZ);
+            StartCoroutine(_leftChunk.RebuildMesh());
+            //Debug.Log("left : " + _leftChunk.name);
+        }
+        // 前面
+        if (blockZ == 0)
+        {
+            if (_frontChunk == null)
+                _frontChunk = ChunkMgr7Perlin.GetChunkByChunkPos(blockX, blockY, blockZ - 1);
+            StartCoroutine(_frontChunk.RebuildMesh());
+            //Debug.Log("front : " + _frontChunk.name);
+        }
+        // 后面
+        if (blockZ == width - 1)
+        {
+            if (_backChunk == null)
+                _backChunk = ChunkMgr7Perlin.GetChunkByChunkPos(blockX, blockY, blockZ + 1);
+            StartCoroutine(_backChunk.RebuildMesh());
+            //Debug.Log("back : " + _backChunk.name);
+        }
+        // 上面
+        if (blockY == height - 1)
+        {
+            if (_topChunk == null)
+                _topChunk = ChunkMgr7Perlin.GetChunkByChunkPos(blockX, blockY + 1, blockZ);
+            StartCoroutine(_topChunk.RebuildMesh());
+            //Debug.Log("top : " + _topChunk.name);
+        }
+        // 下面
+        if (blockY == 0)
+        {
+            if (_bottomChunk == null)
+                _bottomChunk = ChunkMgr7Perlin.GetChunkByWorldPos(blockX, blockY - 1, blockZ);
+            StartCoroutine(_bottomChunk.RebuildMesh());
+            //Debug.Log("bottom : " + _bottomChunk.name);
+        }
+        #endregion
+    }
+
+    public Vector3 GetChunkPos()
+    {
+        return new Vector3(chunkX, chunkY, chunkZ);
+    }
+}
---
+++ Assets/Test/Perlin/Scripte/ChunkMgr7Perlin.cs
@@ -0,0 +1,210 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkMgr7Perlin : MonoBehaviour
+{
+    [Label("玩家")]
+    public Transform player;
+    [Label("图快预制体")]
+    [SerializeField]
+    private GameObject _chunkPrefab;
+    [SerializeField]
+    [Label("点击显示的高亮预制体")]
+    private GameObject _hightBlock;
+
+    [SerializeField]
+    [Label("是否预加载图块")]
+    private bool _preload = true;
+    /// <summary>
+    /// 加载范围以玩家为中心点的正方向边长的一半
+    /// </summary>
+    [SerializeField]
+    [Header("预加载图块范围")]
+    [Range(0, 100)]
+    private int _preloadRange = 60;
+
+    [Label("最大图块高度")]
+    public int _maxHigh = 8;
+
+    /// <summary>
+    /// 保存所有chunk
+    /// </summary>
+    [HideInInspector]
+    private static List<Chunk7Perlin> _chunks = new List<Chunk7Perlin>();
+
+    private static ChunkMgr7Perlin _instance;
+    public static ChunkMgr7Perlin Instance()
+    {
+        return _instance;
+    }
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    private void Update()
+    {
+        if (_preload)
+        {
+            for (float x = player.position.x - _preloadRange; x < player.position.x + _preloadRange; x += Chunk.length)
+            {
+                int chunkX = Mathf.FloorToInt(x / Chunk.length);
+                for (float z = player.position.z - _preloadRange; z < player.position.z + _preloadRange; z += Chunk.width)
+                {
+                    int chunkZ = Mathf.FloorToInt(z / Chunk.width);
+                    for (float y = player.position.y - _preloadRange; y < player.position.y + _preloadRange; y += Chunk.height)
+                    {
+                        int chunkY = Mathf.FloorToInt(y / Chunk.height);
+                        if (GetChunkByChunkPos(chunkX, chunkY, chunkZ) == null)
+                        {
+                            GameObject go = Instantiate(_chunkPrefab, new Vector3(
+                                chunkX * Chunk.length, chunkY * Chunk.width, chunkZ * Chunk.height), Quaternion.identity);
+                            Chunk7Perlin chunk = go.GetComponent<Chunk7Perlin>();
+                            chunk.Init(chunkX, chunkY, chunkZ);
+                            _chunks.Add(chunk);
+                        }
+                    }
+                }
+            }
+
+            SpawnChunk();
+        }
+
+        DestoryChunk();
+        BlockContrller();
+    }
+
+    private void SpawnChunk()
+    {
+        if (Chunk7Perlin.working)
+            return;
+
+        float lastDis = 99999999;
+        Chunk7Perlin target = null;
+        for (int i = 0; i < _chunks.Count; i++)
+        {
+            Chunk7Perlin chunk = _chunks[i];
+            float dis = Vector3.Distance(chunk.transform.position, player.position);
+            if (dis < lastDis)
+            {
+                if (chunk.ready == false)
+                {
+                    lastDis = dis;
+                    target = chunk;
+                }
+            }
+        }
+
+        if (target != null)
+        {
+            target.CreateMap();
+        }
+    }
+
+    private void DestoryChunk()
+    {
+        for (int i = _chunks.Count - 1; i >= 0; i--)
+        {
+            Chunk7Perlin chunk = _chunks[i];
+            float dis = Vector3.Distance(chunk.transform.position, player.position);
+            if (dis > (_preloadRange * 2 + Chunk.width))
+            {
+                _chunks.Remove(chunk);
+                Destroy(chunk.gameObject);
+            }
+        }
+    }
+
+    private void BlockContrller()
+    {
+        RaycastHit hitInfo;
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hitInfo, 10f))
+        {
+            Vector3 pos = hitInfo.point - hitInfo.normal / 2;
+            //Vector3 pos = new Vector3(hitX, hitY, hitZ);
+            _hightBlock.transform.position = DataUtil.CeilToInt(pos);
+
+            if (Input.GetMouseButton(0))
+            {
+                Chunk7Perlin chunk = GetChunkByWorldPos(DataUtil.CeilToInt(pos));
+                chunk.SetBlock(pos, null);
+            }
+            else if (Input.GetKeyDown(KeyCode.Q))
+            {
+                pos = hitInfo.point + hitInfo.normal / 2;
+                Chunk7Perlin chunk = GetChunkByWorldPos(DataUtil.CeilToInt(pos));
+                chunk.SetBlock(pos, BlockMap.GetBlock("TNT"));
+            }
+        }
+        else
+        {
+            _hightBlock.transform.position = new Vector3(10000, 10000, 10000);
+        }
+    }
+
+    #region 获取图块
+    public static Chunk7Perlin GetChunkByWorldPos(int x, int y, int z)
+    {
+        Vector3 pos = new Vector3(x, y, z);
+        return GetChunkByWorldPos(pos);
+    }
+
+    public static Chunk7Perlin GetChunkByWorldPos(Vector3 pos)
+    {
+        for (int i = 0; i < _chunks.Count; i++)
+        {
+            Vector3 chunkPos = _chunks[i].transform.position;
+            if (chunkPos.Equals(pos))
+                return _chunks[i];
+
+            if (pos.x < chunkPos.x || pos.y < chunkPos.y || pos.z < chunkPos.z
+                || pos.x >= chunkPos.x + Chunk.length || pos.y >= chunkPos.y + Chunk.height || pos.z >= chunkPos.z + Chunk.width)
+                continue;
+
+            return _chunks[i];
+        }
+        return null;
+    }
+
+    public static Chunk7Perlin GetChunkByChunkPos(int x, int y, int z)
+    {
+        Vector3 pos = new Vector3(x, y, z);
+        return GetChunkByChunkPos(pos);
+    }
+
+    public static Chunk7Perlin GetChunkByChunkPos(Vector3 pos)
+    {
+        for (int i = 0; i < _chunks.Count; i++)
+        {
+            Vector3 chunkPos = _chunks[i].GetChunkPos();
+            if (chunkPos.Equals(pos))
+                return _chunks[i];
+        }
+        return null;
+    }
+
+    public static bool HasChunkByWorldPos(int x, int y, int z)
+    {
+        return HasChunkByWorldPos(new Vector3(x, y, z));
+    }
+
+    public static bool HasChunkByWorldPos(Vector3 pos)
+    {
+        for (int i = 0; i < _chunks.Count; i++)
+        {
+            Vector3 chunkPos = _chunks[i].transform.position;
+            if (chunkPos.Equals(pos))
+                return true;
+
+            if (pos.x < chunkPos.x || pos.y < chunkPos.y || pos.z < chunkPos.z
+                || pos.x >= chunkPos.x + Chunk.length || pos.y >= chunkPos.y + Chunk.height || pos.z >= chunkPos.z + Chunk.width)
+                continue;
+
+            return true;
+        }
+        return false;
+    }
+    #endregion
+}
